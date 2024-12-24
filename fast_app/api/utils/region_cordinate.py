7199d2e6b15da6be54fd0fb5a0695a83c129570d
ShmuelRozental <rozentalshmuel@gmail.com>
2024-12-24 11:44:38 +0200

    add dictonary of region lat lon

---
+++ fast_app/api/utils/region_cordinate.py
@@ -0,0 +1,14 @@
+REGION_COORDINATES = {
+    "Southeast Asia": {"lat": 13.736717, "lon": 100.523186},  # Bangkok, Thailand
+    "Sub-Saharan Africa": {"lat": -1.292066, "lon": 36.821946},  # Nairobi, Kenya
+    "Eastern Europe": {"lat": 50.450001, "lon": 30.523333},  # Kyiv, Ukraine
+    "Middle East & North Africa": {"lat": 25.276987, "lon": 55.296249},  # Dubai, UAE
+    "Australasia & Oceania": {"lat": -33.868820, "lon": 151.209290},  # Sydney, Australia
+    "North America": {"lat": 37.774929, "lon": -122.419416},  # San Francisco, USA
+    "South America": {"lat": -23.550520, "lon": -46.633308},  # São Paulo, Brazil
+    "East Asia": {"lat": 35.689487, "lon": 139.691711},  # Tokyo, Japan
+    "Central America & Caribbean": {"lat": 19.432608, "lon": -99.133209},  # Mexico City, Mexico
+    "Western Europe": {"lat": 48.856614, "lon": 2.352222},  # Paris, France
+    "South Asia": {"lat": 28.613939, "lon": 77.209023},  # New Delhi, India
+    "Central Asia": {"lat": 41.299496, "lon": 69.240073},  # Tashkent, Uzbekistan
+}
