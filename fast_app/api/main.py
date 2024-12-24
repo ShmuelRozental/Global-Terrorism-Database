4697e9c4a947d469888f61ef3db1513988d50d8c
ShmuelRozental <rozentalshmuel@gmail.com>
2024-12-24 14:29:17 +0200

    fix to run with docker compose

--- fast_app/api/main.py
+++ fast_app/api/main.py
@@ -1,15 +1,13 @@
 from fastapi import FastAPI, Depends
 from fastapi.responses import ORJSONResponse
-
-from fast_app.api.database.init_db import lifespan
+from api.database.init_db import lifespan
 from .routes import router
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
-
 app = FastAPI(default_response_class=ORJSONResponse, lifespan=lifespan)
-
 
 
 app.include_router(router)
