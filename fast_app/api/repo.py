8fddd266a9aad9e7b785f8e869c2386f1b85d6ed
ShmuelRozental <rozentalshmuel@gmail.com>
2024-12-24 11:29:22 +0200

    set the secound query

--- fast_app/api/repo.py
+++ fast_app/api/repo.py
@@ -25,46 +25,28 @@ class EventRepo:
         return [serialize_doc(event) for event in events] 
     
 
-    async def fetch_avg_casualties_by_area(self, area: str = None) -> List[dict]:
-        match_conditions = {}
-
-        # Build the match conditions for city or region
-        if area:
-            match_conditions["$or"] = [
-                {"location.city": area}, 
-                {"location.region": area}  
-            ]
-
+    async def fetch_avg_casualties_by_region(self, top_n: int = 5) -> List[dict]:
+        if top_n is None:
+            top_n = 12
         pipeline = [
-            # Match stage
-            {"$match": match_conditions},
-            
-            # Group stage: Add lat and lon to the results
-            {
-                "$group": {
-                    "_id": {
-                        "$cond": [
-                            {"$ifNull": ["$location.city", False]}, 
-                            "$location.city",  # Group by city
-                            "$location.region"  # Fallback to group by region
-                        ]
-                    },
-                    "lat": {"$first": "$location.lat"},  # Extract latitude
-                    "lon": {"$first": "$location.lon"},  # Extract longitude
-                    "avg_casualties": {
-                        "$avg": {"$add": [{"$multiply": ["$casualties.fatalities", 2]}, "$casualties.injuries"]}
-                    },
-                    "num_events": {"$sum": 1}  # Count the number of events
-                }
-            },
-            
-            # Sort stage: Order by average casualties descending
-            {"$sort": {"avg_casualties": -1}}
-        ]
-        
-        # Execute the aggregation pipeline
+            {"$match": {"location.lat": {"$ne": None}, "location.lon": {"$ne": None}}},
+        {
+            "$group": {
+                "_id": "$location.region",  
+                "lat": {"$first": "$location.lat"}, 
+                "lon": {"$first": "$location.lon"},  
+                "avg_casualties": {
+                    "$avg": {"$add": [{"$multiply": ["$casualties.fatalities", 2]}, "$casualties.injuries"]}
+                },  
+                "total_casualties": {
+                    "$sum": {"$add": [{"$multiply": ["$casualties.fatalities", 2]}, "$casualties.injuries"]}
+                },  
+                "num_events": {"$sum": 1}  
+            }
+        },
+        {"$sort": {"total_casualties": -1}},  
+        {"$limit": top_n}  
+    ]
         events_cursor = self.db.events.aggregate(pipeline)
         events = await events_cursor.to_list(length=None)
-        
-        # Serialize the documents
         return [serialize_doc(event) for event in events]
