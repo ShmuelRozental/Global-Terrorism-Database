5bbf00782bd2ff73802a55164543b7847be1d747
ShmuelRozental <rozentalshmuel@gmail.com>
2024-12-30 16:37:33 +0200

    update

--- search_app/api/app.py
+++ search_app/api/app.py
@@ -1,29 +1,41 @@
 from flask import Flask, jsonify, request
 from datetime import datetime
-from search import search_keywords, search_news, search_historic, search_combined
+from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
+
+es = Elasticsearch(hosts=["http://localhost:9200"])
 
 
 @app.route('/search/keywords', methods=['GET'])
 def search_by_keywords():
     keywords = request.args.get('keywords', '')
     limit = request.args.get('limit', 10, type=int)
-    results = search_keywords(keywords, limit)
-    return jsonify(results)
-
-
-@app.route('/search/news', methods=['GET'])
-def search_news_endpoint():
-    limit = request.args.get('limit', 10, type=int)
-    results = search_news(limit)
+    
+    query = {
+        "query": {
+            "multi_match": {
+                "query": keywords,
+                "fields": ["summary", "location.country", "target_type.target_type_name"]
+            }
+        },
+        "size": limit
+    }
+    response = es.search(index="historic_events", body=query)
+    results = [hit["_source"] for hit in response["hits"]["hits"]]
     return jsonify(results)
 
 
 @app.route('/search/historic', methods=['GET'])
 def search_historic_endpoint():
     limit = request.args.get('limit', 10, type=int)
-    results = search_historic(limit)
+    
+    query = {
+        "query": {"match_all": {}},
+        "size": limit
+    }
+    response = es.search(index="historic_events", body=query)
+    results = [hit["_source"] for hit in response["hits"]["hits"]]
     return jsonify(results)
 
 
@@ -32,8 +44,32 @@ def search_combined_endpoint():
     start_date = request.args.get('start_date', '')
     end_date = request.args.get('end_date', '')
     limit = request.args.get('limit', 10, type=int)
-    results = search_combined(start_date, end_date, limit)
+
+    query = {
+        "query": {
+            "bool": {
+                "must": [],
+                "filter": []
+            }
+        },
+        "size": limit
+    }
+
+    if start_date and end_date:
+        query["query"]["bool"]["filter"].append({
+            "range": {
+                "date": {
+                    "gte": start_date,
+                    "lte": end_date,
+                    "format": "yyyy-MM-dd"
+                }
+            }
+        })
+
+    response = es.search(index="historic_events", body=query)
+    results = [hit["_source"] for hit in response["hits"]["hits"]]
     return jsonify(results)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
